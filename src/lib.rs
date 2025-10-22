c9786b177bdb137c862469e4e93e236e5619fa82
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-22 18:25:38 -0500

    running_subset_of_tests_by_name

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,34 @@
-fn prints_and_returns_10(a: i32) -> i32 {
-    println!("I got the value {}", a);
-    10
+pub fn add_two(a: i32) -> i32 { 
+    a + 2 
 }
 
 #[cfg(test)]
-mod tests {
+mod tests{
     use super::*;
 
-    #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
+    #[test] // cargo test add
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2))
     }
 
     #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3))
+    }
+
+    #[test] // cargo test one_hundred
+    fn one_hundred() {
+        assert_eq!(102, add_two(100))
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4)
+    }
+
+    #[test]
+    #[ignore] // cargo test -- --ignored
+    fn expensive_test() {
+        // code that takes an hour to run
     }
 }
