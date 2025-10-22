f9aac181caccb9e02090c3655d3c5137c8388236
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-22 18:44:15 -0500

    unit_tests_and_testing_private_fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,17 @@
 pub fn add_two(a: i32) -> i32 { 
-    a + 2 
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32 ) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
 mod tests{
     use super::*;
 
-    #[test] // cargo test add
-    fn add_two_and_two() {
-        assert_eq!(4, add_two(2))
-    }
-
     #[test]
-    fn add_three_and_two() {
-        assert_eq!(5, add_two(3))
-    }
-
-    #[test] // cargo test one_hundred
-    fn one_hundred() {
-        assert_eq!(102, add_two(100))
-    }
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4)
-    }
-
-    #[test]
-    #[ignore] // cargo test -- --ignored
-    fn expensive_test() {
-        // code that takes an hour to run
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
